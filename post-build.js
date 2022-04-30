4683cf07ea46266efc05153cfc91c520a8c39ce6
dmitritruf <gttidm2@gmail.com>
2022-04-30 00:23:29 -0700

    update post build

--- post-build.js
+++ post-build.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 // Path to locales directory
 const src = "./markdown-blogs";
 // Path to default-lambda destination
-const dest = "./.next/server/markdown-blogs";
+const dest = "./.next/server/pages/markdown-blogs";
 // Copy Files over recursively
 fs.copySync(src, dest, { recursive: true });
 console.log("Locale directory was copied successfully");
