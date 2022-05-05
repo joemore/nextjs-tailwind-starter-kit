826dbf94cc59e346cb0da4dadf9a775b2eb57801
Joe Gilmore <joe@joemore.com>
2022-05-06 08:59:58 +1000

    Aded sitemap generator code (JS not TS yet)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-var path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  "target": "experimental-serverless-trace",
+  
+  // The following allows our site map to be generated at build time
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./lib/sitemap-generator");
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
